0888e3e408b10921520f2d4e9e2038faf5b9b5b5
Jamie <j@dnbr.uk>
2025-10-30 14:11:57 +0000

    fix to single image

--- src/routes/archive/record/[id]/+page.server.ts
+++ src/routes/archive/record/[id]/+page.server.ts
@@ -3,12 +3,11 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
   const sql = `
-		SELECT r.title, r.id, f.id as file_id, r.detail, ST_asGeoJSON(r.location_geom)::json as geojson, r.downloadable,
-		r.date_year, r.date_month, r.date_day, f.mime_type as file_mime,  r.collection_id, 
+		SELECT r.title, r.id, r.file_id, r.detail, ST_asGeoJSON(r.location_geom)::json as geojson, r.downloadable,
+		r.date_year, r.date_month, r.date_day, r.file_mime,  r.collection_id, 
 		r.public, c.name as colname, c.slug as colslug, r.location_estimated, r.image_transform::json
 		from records r 
 		LEFT JOIN collections c on r.collection_id = c.id
-		LEFT JOIN files f on r.file_id = f.id
 		where r.id = $1
 		LIMIT 1
 	`;
