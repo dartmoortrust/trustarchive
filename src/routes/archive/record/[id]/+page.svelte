4ce271679f7088ceae6a7d561ddd4fa5101d4a3c
Jamie <j@dnbr.uk>
2025-12-19 13:19:28 +0000

    audio image fix

--- src/routes/archive/record/[id]/+page.svelte
+++ src/routes/archive/record/[id]/+page.svelte
@@ -27,7 +27,7 @@
     description={data.record.title || "Dartmoor Trust Archive"}
   />
   <div class="md:hidden">
-    <ArchiveImage record={data.record} />
+    <ArchiveImage record={data.record} size={300} crop={true} />
   </div>
   <Container>
     <Flex>
@@ -68,7 +68,7 @@
             <VideoPlayer record={data.record} />
           {:else}
             <MobileHidden>
-              <ArchiveImage record={data.record} size={500} crop={false} lightbox={true} />
+              <ArchiveImage record={data.record} size={500} crop={false} />
             </MobileHidden>
           {/if}
           <Map geojson={data.record.geojson} estimated={data.record.location_estimated} />
