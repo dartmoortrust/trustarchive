32a0448736159853b6ece1964eeb12cbb7f9b835
Jamie <j@dnbr.uk>
2025-12-23 12:55:00 +0000

    adding in bigger edit box

--- src/lib/ui/form/TextArea.svelte
+++ src/lib/ui/form/TextArea.svelte
@@ -10,6 +10,7 @@
     <Label {label} name={other.name} />
   {/if}
   <textarea
+    rows="15"
     {...other}
     class="px-3 py-2 bg-white border-2 border-gray-300 rounded-lg transition-colors disabled:opacity-50 disabled:cursor-not-allowed {resize
       ? 'resize-y'
