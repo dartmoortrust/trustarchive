ae6faa93f6c59a8bd7738a72688c514802cd8be0
Jamie <j@dnbr.uk>
2026-01-23 11:55:48 +0000

    adding in sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,30 @@
+import { sql } from "bun";
+
+export async function GET() {
+    const pages = await sql`SELECT id from files order by id asc`
+    console.log(pages)
+	return new Response(
+		`
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<urlset
+			xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+			xmlns:xhtml="http://www.w3.org/1999/xhtml"
+			xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+			xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+			xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+			xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
+		>
+			${pages.map((page:any) => `
+            <url>
+                <loc>/archive/record/${page.id}</loc>
+                <changefreq>yearly</changefreq>
+            </url>
+            `).join('')}
+		</urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
